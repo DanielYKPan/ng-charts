b735e229d8c307ade1063eaac1c194a7a6d8638e
Yaokun Pan <myron.yk.pan@gmail.com>
2017-06-11 14:41:03 +1000

    Added chart.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@
  * app.component
  */
 
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Chart } from 'chart.js';
 
 @Component({
     selector: 'yk-app',
@@ -12,9 +13,45 @@ import { Component, OnInit } from "@angular/core";
 
 export class AppComponent implements OnInit {
 
+    @ViewChild('myCanvas') myCanvas: any;
+
+    private ctx: any;
+    private cvs: any;
+
     constructor() {
     }
 
     ngOnInit(): void {
+        this.ctx = this.myCanvas.nativeElement.getContext('2d');
+        this.cvs = this.myCanvas.nativeElement;
+
+        new Chart(this.ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {}
+        });
     }
 }
